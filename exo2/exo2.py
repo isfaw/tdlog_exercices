587cb44419d7fee214e9f64794d3156852ba15d5
isfaw <148464292+isfaw@users.noreply.github.com>
2024-09-26 13:42:32 +0100

    Update exo2.py

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -1,30 +1,34 @@
-"""
-Complete the solution so that it returns true if the first argument(string)
-passed in ends with the 2nd argument (also a string).
+def solution(string, ending):
+    return string.endswith(ending)
+import unittest
 
-Examples:
+class TestSolution(unittest.TestCase):
 
-    solution('abc', 'bc') # returns true
-    solution('abc', 'd') # returns false
-"""
+    def test_fixed_tests_true(self):
+        fixed_tests_True = (
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails"),
+        )
+        for s, e in fixed_tests_True:
+            with self.subTest(s=s, e=e):
+                self.assertTrue(solution(s, e))
 
-"""
-Create unit test using those cases:
-fixed_tests_True = (
-    ( "samurai", "ai"    ),
-    ( "ninja",   "ja"    ),
-    ( "sensei",  "i"     ),
-    ( "abc",     "abc"   ),
-    ( "abcabc",  "bc"    ),
-    ( "fails",   "ails"  ),
-)
+    def test_fixed_tests_false(self):
+        fixed_tests_False = (
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs"),
+        )
+        for s, e in fixed_tests_False:
+            with self.subTest(s=s, e=e):
+                self.assertFalse(solution(s, e))
 
-fixed_tests_False = (
-    ( "sumo",    "omo"   ),
-    ( "samurai", "ra"    ),
-    ( "abc",     "abcd"  ),
-    ( "ails",    "fails" ),
-    ( "this",    "fails" ),
-    ( "spam",    "eggs"  )
-)
-"""
+if __name__ == "__main__":
+    unittest.main()
